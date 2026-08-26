f27cb0afb2f6078cf0516c33c42e9de9772f6f63
LisaYllander92 <Lisaylander92@gmail.com>
2026-08-26 14:38:45 +0200

    test backend fastapi and frontend streamlit

--- backend/src/backend/api.py
+++ backend/src/backend/api.py
@@ -3,6 +3,6 @@ from backend.data_processing import df
 
 app =FastAPI()
 
-@app.get("/pokemons/stats")
+@app.get("/pokemon/stats")
 async def show_data():
     return df.to_dict(orient="records")
---
+++ frontend/src/frontend/dashboard.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import httpx 
+
+def main():
+    st.markdown("# PokeDash")
+
+    stats = httpx.get("http://127.0.0.1:8000/pokemon/stats", timeout=30).json()
+    st.dataframe(stats)
+
+if __name__ == "__main__":
+    main()
