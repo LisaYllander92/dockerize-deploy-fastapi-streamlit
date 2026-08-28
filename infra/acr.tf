d616b9fd2d49b78af9d9b083c7e38803d501abf1
LisaYllander92 <Lisaylander92@gmail.com>
2026-08-28 10:57:15 +0200

    update files - to test later

--- infra/acr.tf
+++ infra/acr.tf
@@ -1,7 +1,10 @@
 resource "azurerm_container_registry" "acr" {
-  name = "${var.acr_name}${random_string.suffix.result}"
-  location = azurerm_resource_group.rg.location
+  name                = "${var.acr_name}${random_string.suffix.result}"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku = "Basic"
-  admin_enabled = true
+  sku                 = "Basic"
+  admin_enabled       = true
 }
+
+# full dependency graph for the acr
+# terraform apply -auto-approve -target=azurerm_container_registry.acr
